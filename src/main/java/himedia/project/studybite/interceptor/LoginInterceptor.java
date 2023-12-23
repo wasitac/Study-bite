8913e00db2dd4634a984270dc9f077557f81b951
wasitac <67859497+wasitac@users.noreply.github.com>
2023-12-23 16:11:29 +0900

    docs: 자동정렬

--- src/main/java/himedia/project/studybite/interceptor/LoginInterceptor.java
+++ src/main/java/himedia/project/studybite/interceptor/LoginInterceptor.java
@@ -8,25 +8,28 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
  * 모든 요청마다 로그인 여부를 확인하기 위한 인터셉터입니다.
+ * 
  * @author 이지홍
  *
  */
 public class LoginInterceptor extends HandlerInterceptorAdapter {
-	
+
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) 
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		return super.preHandle(request, response, handler);
 	}
+
 	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) 
-			throws Exception {
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
 		super.postHandle(request, response, handler, modelAndView);
 	}
+
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) 
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		super.afterCompletion(request, response, handler, ex);
 	}
-	
+
 }
