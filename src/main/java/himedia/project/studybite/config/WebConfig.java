00a5a5ec53508752ee3daab0b0a5dfa4ca23d4f5
wasitac <67859497+wasitac@users.noreply.github.com>
2023-12-23 15:42:13 +0900

    init: interceptor

--- src/main/java/himedia/project/studybite/config/WebConfig.java
+++ src/main/java/himedia/project/studybite/config/WebConfig.java
@@ -1,23 +1,23 @@
-//package himedia.project.studybite.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import himedia.project.studybite.interceptor.LoginInterceptor;
-//
-//public class WebConfig implements WebMvcConfigurer {
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(loginInterceptor())
-//			.order(1)
-//			.addPathPatterns("/**")
-//			.excludePathPatterns("/css/**", "/*.ico", "/error");
-//	}
-//	
-//	@Bean
-//	public LoginInterceptor loginInterceptor() {
-//		return new LoginInterceptor();
-//	}
-//
-//}
+package himedia.project.studybite.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import himedia.project.studybite.interceptor.LoginInterceptor;
+
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(loginInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/css/**", "/*.ico", "/error");
+	}
+	
+	@Bean
+	public LoginInterceptor loginInterceptor() {
+		return new LoginInterceptor();
+	}
+
+}
