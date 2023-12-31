c43fc3a2c4856501f5e10d1eb57d1a7b1204a61b
wasitac <67859497+wasitac@users.noreply.github.com>
2023-12-31 15:44:56 +0900

    Feat: constructor

--- src/main/java/himedia/project/studybite/domain/User.java
+++ src/main/java/himedia/project/studybite/domain/User.java
@@ -1,16 +1,21 @@
 package himedia.project.studybite.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 /**
  * @author 이지홍
  */
 @Setter @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	private Long userId;
 	private Integer role;
 	private String userName;
-//	private String password;
+	private String password;
 	private String email;
 	private String phone;
 }
