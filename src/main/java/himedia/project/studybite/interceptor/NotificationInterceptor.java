3e5788e38d7661725410404e3bdc102de257e664
wasitac <67859497+wasitac@users.noreply.github.com>
2024-01-14 00:25:14 +0900

    Fix: notificationInterceptor
    기본 생성자가 없어서 생기는 error해결

--- src/main/java/himedia/project/studybite/interceptor/NotificationInterceptor.java
+++ src/main/java/himedia/project/studybite/interceptor/NotificationInterceptor.java
@@ -7,31 +7,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import himedia.project.studybite.dto.Notification;
 import himedia.project.studybite.dto.User;
 import himedia.project.studybite.service.NotificationService;
-import lombok.RequiredArgsConstructor;
 
 /**
  * 요청때 마다 알림을 갱신하기 위한 인터셉터
+ * 
  * @author 이지홍
  */
-@RequiredArgsConstructor
 public class NotificationInterceptor implements HandlerInterceptor {
 	private final NotificationService notificationService;
+
+	@Autowired
+	public NotificationInterceptor(NotificationService notificationService) {
+		this.notificationService = notificationService;
+	}
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		HttpSession session = request.getSession(false);
-		Optional<User> user = Optional.ofNullable((User)(session.getAttribute("user")));
+		Optional<User> user = Optional.ofNullable((User) (session.getAttribute("user")));
 
-		if(user.isPresent()) {			
+		if (user.isPresent()) {
 			List<Notification> notifications = notificationService.getNotifications(user.get());
 			session.setAttribute("notifications", notifications);
 		}
-		
 		return true;
 	}
 }
