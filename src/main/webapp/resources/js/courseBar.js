ec6ea35451712926769afd240f4d68d78bb4b625
wasitac <67859497+wasitac@users.noreply.github.com>
2024-01-14 19:27:01 +0900

    Fix: courseBar
    클릭시 이미지 표시 수정

--- src/main/webapp/resources/js/courseBar.js
+++ src/main/webapp/resources/js/courseBar.js
@@ -3,20 +3,15 @@ var linkConcated = linkSplit[0] + "//" + linkSplit[2] + "/" + linkSplit[3] + "/"
 
 document.querySelectorAll(".courseBar").forEach((link) => {
 	if (link.href === window.location.href || link.href === linkConcated) {
-
-    link.classList.add("border-bottom", "border-primary", "border-2");
-    link.classList.remove("link-secondary");
-    const parent = document.querySelector(".border-primary img");
-    var image = parent.src;
-    console.log("image : " + image);
-       
-    var srcSplit = image.split(".");
-    var concated = image;
-    
-    if(srcSplit[0].slice(-1) !== "c")
-        	 concated = srcSplit[0] + "c." + srcSplit[1];
-    
-    console.log("concated : " + concated);      
-    parent.src = concated;          
+	    link.classList.add("border-bottom", "border-primary", "border-2");
+	    link.classList.remove("link-secondary");
+	    
+	    const parent = document.querySelector(".border-primary img");
+	    var imagePath = parent.src;
+	    
+        if(imagePath.indexOf("c.png") == -1)
+        	imagePath = imagePath.replace('.png', 'c.png');
+        	
+        parent.src = imagePath;       
     }
 });
